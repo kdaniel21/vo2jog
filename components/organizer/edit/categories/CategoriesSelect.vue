92e4bcc4fe31d59adbb9a0ddcaf8119a2055438a
Daniel Kiss <kissd621@gmail.com>
2020-11-23 18:34:42 +0100

    Fixed: selected categoryItem is deleted after it's subcategory was deleted

--- components/organizer/edit/categories/CategoriesSelect.vue
+++ components/organizer/edit/categories/CategoriesSelect.vue
@@ -32,28 +32,42 @@ export default {
       subcategoryItems: [],
     };
   },
+  computed: {
+    availableSubcategories() {
+      return new Map(
+        this.subcategoryItems.map(subcategory => [
+          subcategory.name,
+          subcategory.categoryItems,
+        ])
+      );
+    },
+  },
   watch: {
     async selectedMainCategories(val) {
       if (!val) return;
 
       await this.fetchSubcategories();
+      this.removeUnusedSubcategories();
     },
   },
   created() {
     // Parse & Prefill Categories from vuex
     const { categories } = this.$store.state.organizer.events.selectedEvent;
     if (!categories.length) return;
-
     categories.forEach(category => {
       if (category.category === 'main') {
         this.selectedMainCategories = [...category.value];
         return;
       }
-      this.selectedSubcategories[category.category] = [...category.value];
+      this.selectedSubcategories = {
+        ...this.selectedSubcategories,
+        [category.category]: category.value,
+      };
     });
   },
   methods: {
     ...mapActions('organizer/events', ['updateEvent']),
+    // Fetch all subcategories based on the selected main categories
     async fetchSubcategories() {
       const params = { main: this.selectedMainCategories.toString() };
       const res = await this.$axios.get('/api/categories/subcategories', {
@@ -62,6 +76,20 @@ export default {
 
       if (!res.data.data.length) return;
       this.subcategoryItems = [...res.data.data];
+    },
+    // Clean the selectedSubcategories object from entries that are not available anymore
+    removeUnusedSubcategories() {
+      Object.keys(this.selectedSubcategories).forEach(key => {
+        // Remove subcategories that are not available anymore
+        if (!this.availableSubcategories.has(key)) {
+          return delete this.selectedSubcategories[key];
+        }
+
+        // Keep items only that are still available
+        this.selectedSubcategories[key] = this.selectedSubcategories[
+          key
+        ].filter(item => this.availableSubcategories.get(key).includes(item));
+      });
     },
     async saveCategories() {
       const categories = {
