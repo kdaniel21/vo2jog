2b3cafcf53ae93e82622eb35862b6431f1b4ca2d
Daniel Kiss <kissd621@gmail.com>
2020-11-28 08:52:11 +0100

    Added reset password feature

---
+++ components/organizer/auth/reset-password/ResetPasswordForm.vue
@@ -0,0 +1,86 @@
+<template>
+  <b-form id="reset-password-form" @submit.prevent="resetPassword">
+    <form-group
+      label="Password"
+      label-for="password-input"
+      :validator="$v.form.password"
+    >
+      <b-form-input
+        id="password-input"
+        v-model="form.password"
+        slot-scope="{ attrs, listeners }"
+        placeholder="New Password"
+        v-bind="attrs"
+        v-on="listeners"
+      ></b-form-input>
+    </form-group>
+
+    <form-group
+      label="Password Confirm"
+      label-for="password-confirm-input"
+      :validator="$v.form.passwordConfirm"
+    >
+      <b-form-input
+        id="password-confirm-input"
+        v-model="form.passwordConfirm"
+        slot-scope="{ attrs, listeners }"
+        placeholder="Your Password Again"
+        v-bind="attrs"
+        v-on="listeners"
+      ></b-form-input>
+    </form-group>
+
+    <b-button variant="primary" type="Submit" class="btn-block-xs-only"
+      >Set Password</b-button
+    >
+  </b-form>
+</template>
+
+<script>
+import { required, sameAs, minLength } from 'vuelidate/lib/validators';
+import toaster from '@/mixins/toaster';
+
+export default {
+  name: 'ResetPasswordForm',
+  mixins: [toaster],
+  props: {
+    token: { type: String, default: null },
+  },
+  data() {
+    return {
+      form: {
+        password: null,
+        passwordConfirm: null,
+      },
+    };
+  },
+  validations: {
+    form: {
+      password: {
+        required,
+        minLength: minLength(8),
+      },
+      passwordConfirm: {
+        required,
+        minLength: minLength(8),
+        sameAs: sameAs(form => form.password),
+      },
+    },
+  },
+  methods: {
+    async resetPassword() {
+      try {
+        const { token } = this;
+        const { password, passwordConfirm } = this.form;
+        const data = { token, password, passwordConfirm };
+        await this.$axios.post('/api/organizers/reset-password', data);
+
+        this.successToast('Password successfully changed. Now you can log in!');
+        setTimeout(() => this.$router.push('/organizers/login'), 2000);
+      } catch {
+        this.errorToast('Password could not be changed! Please try again.');
+      }
+    },
+  },
+};
+</script>
---
+++ pages/organizers/reset-password/_token/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="reset-password">
+    <reset-password-form
+      v-if="passwordResetToken"
+      :token="passwordResetToken"
+    />
+  </div>
+</template>
+
+<script>
+import ResetPasswordForm from '@/components/organizer/auth/reset-password/ResetPasswordForm';
+
+export default {
+  components: { ResetPasswordForm },
+  data() {
+    return {
+      passwordResetToken: null,
+    };
+  },
+  created() {
+    this.passwordResetToken = this.$route.params.token;
+  },
+};
+</script>
