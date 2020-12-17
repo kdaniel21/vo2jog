a956f25e5ca4fe5b3331372dbf674dd4a922ec2c
Daniel Kiss <kissd621@gmail.com>
2020-12-17 10:45:57 +0100

    Started implementing signup card

---
+++ components/main/event-page/EventPageSignupCard.vue
@@ -0,0 +1,42 @@
+<template>
+  <b-card id="event-page-signup-card" no-body>
+    <b-card-header>
+      <b-badge
+        v-for="category in categories"
+        :key="category"
+        class="text-capitalize"
+      >
+        {{ category }}
+      </b-badge>
+    </b-card-header>
+
+    <b-card-body>
+      <h5>Sign up now!</h5>
+      <h5>From {{ lowestCompetitionPrice }}&euro;</h5>
+
+      <b-button variant="outline-primary" pill>Sign Up</b-button>
+      <b-button varinat="outline-secondary" pill>Share</b-button>
+    </b-card-body>
+  </b-card>
+</template>
+
+<script>
+import displayEvent from '@/mixins/main/display-event';
+
+export default {
+  name: 'EventPageSignupCard',
+  mixins: [displayEvent],
+  props: {
+    event: { type: Object, default: null },
+  },
+  computed: {
+    lowestCompetitionPrice() {
+      const lowest = 0;
+
+      return lowest;
+    },
+  },
+};
+</script>
+
+<style></style>
