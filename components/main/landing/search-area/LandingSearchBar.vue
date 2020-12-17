26b2fe5e3ca548fe5820a724ed85fd843d10c6fd
Daniel Kiss <kissd621@gmail.com>
2020-12-17 10:32:02 +0100

    Implemented search for landing page

--- components/main/landing/search-area/LandingSearchBar.vue
+++ components/main/landing/search-area/LandingSearchBar.vue
@@ -1,11 +1,14 @@
 <template>
   <b-card id="search-bar" body-class="p-2">
-    <b-form inline @submit.prevent>
+    <b-form inline @submit.prevent="search">
       <b-input-group class="flex-grow-1 mr-0 mr-sm-3 mb-2 mb-sm-0">
         <b-input-group-prepend is-text>
           <fa icon="search" fixed-width />
         </b-input-group-prepend>
-        <b-input placeholder="Search for 'Triathlon in Budapest'"></b-input>
+        <b-input
+          v-model="searchString"
+          placeholder="Search for 'Triathlon in Budapest'"
+        ></b-input>
       </b-input-group>
 
       <b-button type="submit" variant="primary" class="btn-block-xs-only" pill
@@ -18,6 +21,16 @@
 <script>
 export default {
   name: 'LandingSearchBar',
+  data() {
+    return {
+      searchString: null,
+    };
+  },
+  methods: {
+    search() {
+      this.$router.push({ path: '/search', query: { q: this.searchString } });
+    },
+  },
 };
 </script>
 
