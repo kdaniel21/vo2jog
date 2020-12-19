cfa55011c0893bc8b48f4a5d26ce8a6ed3204a4c
Daniel Kiss <kissd621@gmail.com>
2020-12-19 10:03:34 +0100

    Refactored event card

---
+++ components/shared/EventCard.vue
@@ -0,0 +1,60 @@
+<template>
+  <nuxt-link v-if="event" :to="`/e/${event.id}`">
+    <div id="event-card" class="card is-flex is-flex-direction-column">
+      <div class="card-image">
+        <figure class="image is4by3">
+          <img :src="event.image" alt="Event Image" />
+        </figure>
+      </div>
+
+      <div
+        class="card-content is-flex-grow-1 pt-1 pb-2 px-2 is-flex is-flex-direction-column is-justify-content-space-between"
+      >
+        <div>
+          <h5 class="is-size-7 is-uppercase">{{ startDate }}</h5>
+          <h3 class="subtitle has-text-weight-medium mb-0">{{ event.name }}</h3>
+          <div>
+            <b-icon icon="map-marker-alt" />
+            <span class="">{{ location }}</span>
+          </div>
+        </div>
+
+        <!-- CATEGORIES -->
+        <div v-if="categories" class="is-clipped">
+          <span
+            v-for="category in categories"
+            :key="category"
+            class="tag is-primary mr-1 is-capitalized"
+          >
+            {{ category }}
+          </span>
+        </div>
+      </div>
+    </div>
+  </nuxt-link>
+</template>
+
+<script>
+import displayEvent from '@/mixins/main/display-event';
+
+export default {
+  name: 'EventCard',
+  mixins: [displayEvent],
+  props: {
+    event: {
+      type: Object,
+      default: null,
+    },
+  },
+};
+</script>
+
+<style scoped>
+#event-card {
+  width: min(80vw, 18rem);
+  height: 290px;
+}
+.icon {
+  width: inherit;
+}
+</style>
