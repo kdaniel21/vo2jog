45272f0be08ae1baa7461efff1ab73070a22e1e6
Daniel Kiss <kissd621@gmail.com>
2020-12-21 12:31:05 +0100

    Changed to light version and moved to another folder

---
+++ components/shared/event/CategoryList.vue
@@ -0,0 +1,20 @@
+<template>
+  <div v-if="categories" class="is-clipped">
+    <span
+      v-for="category in categories"
+      :key="category"
+      class="tag is-primary mr-1 is-capitalized is-light"
+    >
+      {{ category }}
+    </span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CategoryList',
+  props: { categories: { type: Array, default: null } },
+};
+</script>
+
+<style></style>
