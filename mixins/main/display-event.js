a780e6b534605c6d4da09d20683fa3d22dc1dc80
Daniel Kiss <kissd621@gmail.com>
2020-12-21 13:15:15 +0100

    Changed image cover property name

--- mixins/main/display-event.js
+++ mixins/main/display-event.js
@@ -4,7 +4,7 @@ export default {
       maxNumOfCategories: 8,
       priorityCategories: ['main', 'terrain', 'distance'],
       placeholderImage:
-        'http://localhost:4000/events/img/event-5fa4e65399fadd4f0ebf1d8c-1605541380244.jpeg',
+        'http://localhost:4000/events/img/event-5fa4e65399fadd4f0ebf1d8c-1608548616254.jpeg',
     };
   },
   computed: {
@@ -12,7 +12,7 @@ export default {
       return `/e/${this.event.id}`;
     },
     image() {
-      return this.event.image || this.placeholderImage;
+      return this.event.imageCover || this.placeholderImage;
     },
     startDate() {
       if (!this.event.startDate) return 'No date yet.';
