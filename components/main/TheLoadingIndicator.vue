84535e03438318991885599915c4a357e1596d90
Daniel Kiss <kissd621@gmail.com>
2020-12-23 13:40:13 +0100

    Added custom loading component

---
+++ components/main/TheLoadingIndicator.vue
@@ -0,0 +1,24 @@
+<template>
+  <b-loading id="the-loading-indicator" v-model="isLoading"></b-loading>
+</template>
+
+<script>
+export default {
+  name: 'TheLoadingIndicator',
+  data() {
+    return {
+      isLoading: false,
+    };
+  },
+  methods: {
+    start() {
+      this.isLoading = true;
+    },
+    finish() {
+      this.isLoading = false;
+    },
+  },
+};
+</script>
+
+<style></style>
--- nuxt.config.js
+++ nuxt.config.js
@@ -141,6 +141,8 @@ export default {
     },
   },
 
+  loading: '~/components/main/TheLoadingIndicator',
+
   // Fontawesome module configuration
   fontawesome: {
     component: 'fa',
