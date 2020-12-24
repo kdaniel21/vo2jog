099f0a01cda6e8e12036cfbe59c967892e0c3046
Daniel Kiss <kissd621@gmail.com>
2020-12-24 09:40:53 +0100

    Added organizer navbar

---
+++ components/organizer/OrganizerNavbar.vue
@@ -0,0 +1,52 @@
+<template>
+  <b-navbar id="organizer-navbar">
+    <template #brand>
+      <b-navbar-item tag="nuxt-link" to="/organizers">
+        {{ $t('navbar.brand') }}
+      </b-navbar-item>
+    </template>
+
+    <template #end>
+      <b-navbar-dropdown>
+        <template #label>{{ user.name }}</template>
+
+        <b-navbar-item tag="nuxt-link" to="/organizers/profile">
+          <b-icon icon="user-circle" />
+          <span>{{ $t('organizer_navbar.public_profile') }}</span>
+        </b-navbar-item>
+
+        <b-navbar-item tag="nuxt-link" to="/organizers/profile">
+          <b-icon icon="user-cog" />
+          <span>{{ $t('organizer_navbar.account') }}</span>
+        </b-navbar-item>
+
+        <b-navbar-item @click="$auth.logout">
+          <b-icon icon="sign-out-alt" />
+          <span>{{ $t('organizer_navbar.sign_out') }}</span>
+        </b-navbar-item>
+      </b-navbar-dropdown>
+
+      <b-navbar-item>
+        <b-image id="avatar" :src="user.avatar" rounded />
+      </b-navbar-item>
+    </template>
+  </b-navbar>
+</template>
+
+<script>
+export default {
+  name: 'OrganizerNavbar',
+  computed: {
+    user() {
+      return this.$auth.user;
+    },
+  },
+};
+</script>
+
+<style>
+#avatar > img {
+  width: 1.75rem;
+  height: 1.75rem;
+}
+</style>
