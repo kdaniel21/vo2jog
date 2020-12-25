be9fd8df02ab8a7b47820c16b3b7cffb28f68855
Daniel Kiss <kissd621@gmail.com>
2020-12-25 14:52:03 +0100

    Removed unused components

---
+++ pages/organizers/edit/information.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="container">
+    <div class="mb-5">
+      <h1 class="title mb-2">{{ $t('organizer.info.title') }}</h1>
+      <i18n path="organizer.info.description" tag="p">
+        <template #link>
+          <nuxt-link to="/organizers/edit/details">
+            {{ $t('organizer.info.click_here') }}
+          </nuxt-link>
+        </template>
+      </i18n>
+    </div>
+
+    <basic-information />
+  </div>
+</template>
+
+<script>
+export default {
+  layout: 'organizer',
+};
+</script>
+
+<style></style>
