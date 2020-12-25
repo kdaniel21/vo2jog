454d981054a03c9a9baae880ba11ec5b51d9508d
Daniel Kiss <kissd621@gmail.com>
2020-12-25 17:09:59 +0100

    Encapsulated right aligned button

---
+++ components/organizer/edit/information/BasicInformation.vue
@@ -0,0 +1,89 @@
+<template>
+  <div id="basic-information">
+    <h2 class="title">{{ $t('organizer.info.basic_information') }}</h2>
+
+    <form-group
+      :validator="$v.form.name"
+      :label="$t('organizer.info.event_name')"
+      horizontal
+    >
+      <b-input
+        v-model="form.name"
+        :placeholder="$t('organizer.info.name_placeholder')"
+        @input="$v.form.name.$touch()"
+      />
+    </form-group>
+
+    <form-group :label="dateLabel" horizontal>
+      <b-datepicker
+        icon="calendar-alt"
+        :placeholder="$t('organizer.info.select_date')"
+      />
+    </form-group>
+
+    <form-group
+      v-if="form.multiDay"
+      :label="$t('organizer.info.end_date')"
+      horizontal
+    >
+      <b-datepicker
+        v-model="form.endDate"
+        icon="calendar-alt"
+        :placeholder="$t('organizer.info.select_date')"
+      />
+    </form-group>
+
+    <!-- EVENT LENGTH -->
+    <form-group :label="$t('organizer.info.event_length')" horizontal>
+      <div class="is-flex is-flex-direction-column">
+        <b-radio
+          v-model="form.multiDay"
+          name="length"
+          class="mb-2"
+          :native-value="false"
+        >
+          {{ $t('organizer.info.single_day') }}
+        </b-radio>
+        <b-radio v-model="form.multiDay" name="length" :native-value="true">
+          {{ $t('organizer.info.multi_day') }}
+        </b-radio>
+      </div>
+    </form-group>
+
+    <button-right @click="onSave" />
+  </div>
+</template>
+
+<script>
+import { required } from 'vuelidate/lib/validators';
+import { alphaNumWhiteDiacritic } from '@/plugins/vuelidate/custom-validators';
+
+export default {
+  name: 'BasicInformation',
+  data() {
+    return {
+      form: {
+        name: this.$store.state.organizer.events.selectedEvent.name,
+        startDate: this.$store.state.organizer.events.selectedEvent.startDate,
+        endDate: this.$store.state.organizer.events.selectedEvent.endDate,
+        multiDay: !!this.$store.state.organizer.events.selectedEvent.endDate,
+      },
+    };
+  },
+  validations: {
+    form: {
+      name: { required, alphaNumWhiteDiacritic },
+    },
+  },
+  computed: {
+    dateLabel() {
+      return this.form.multiDay
+        ? this.$t('organizer.info.start_date')
+        : this.$t('common.date');
+    },
+  },
+  methods: {
+    onSave() {},
+  },
+};
+</script>
---
+++ components/organizer/shared/ButtonRight.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="is-flex is-justify-content-end">
+    <b-button v-bind="$attrs" :type="type" @click="$emit('click')">
+      <slot>{{ $t('common.save') }}</slot>
+    </b-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ButtonRight',
+  props: {
+    type: { type: String, default: 'is-primary' },
+  },
+};
+</script>
+
+<style></style>
