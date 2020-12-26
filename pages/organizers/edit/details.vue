b7a19d4d33203b6d459d9c6d7ac43ba041572df8
Daniel Kiss <kissd621@gmail.com>
2020-12-26 13:47:04 +0100

    Created details header part

---
+++ pages/organizers/edit/details.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container mb-6">
+    <!-- TITLE -->
+    <div class="mb-5">
+      <h1 class="title mb-2">{{ $t('organizer.details.title') }}</h1>
+      <i18n path="organizer.details.description" tag="p">
+        <span slot="proverb" class="is-italic has-text-primary">
+          {{ $t('organizer.details.details_proverb') }}
+        </span>
+      </i18n>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  layout: 'organizer',
+};
+</script>
+
+<style></style>
