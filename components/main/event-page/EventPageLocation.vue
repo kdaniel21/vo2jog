4faa6f902f28493a2a9425ef64fdd75c198e1232
Daniel Kiss <kissd621@gmail.com>
2020-12-26 09:52:05 +0100

    Encapsulated location data

--- components/main/event-page/EventPageLocation.vue
+++ components/main/event-page/EventPageLocation.vue
@@ -3,20 +3,7 @@
     <div class="card-content">
       <h2 class="title is-4">{{ $t('common.location') }}</h2>
 
-      <div>
-        <span class="has-text-weight-medium">
-          {{ $t('common.address') }}:
-        </span>
-        <span>{{ address }}</span>
-      </div>
-
-      <div>
-        <span class="has-text-weight-medium">
-          {{ $t('event.coordinates') }}:
-        </span>
-        <span>{{ location.coordinates[1] }}&#176;,</span>
-        <span>{{ location.coordinates[0] }}&#176;</span>
-      </div>
+      <location-data :location="location" />
 
       <div class="is-flex is-justify-content-end mt-1">
         <b-button
@@ -41,34 +28,10 @@
 </template>
 
 <script>
-import LocationMap from '@/components/shared/LocationMap';
-
 export default {
   name: 'EventPageLocation',
-  components: { LocationMap },
   props: { location: { type: Object, default: null } },
-  data() {
-    return {
-      addressElements: [
-        { name: 'houseNumber', append: '.' },
-        { name: 'street', append: ',' },
-        { name: 'city', append: '' },
-        { name: 'postalCode', append: ',' },
-        { name: 'county', append: ',' },
-        { name: 'state', append: ',' },
-        { name: 'countryName', append: '' },
-      ],
-    };
-  },
   computed: {
-    address() {
-      return this.addressElements.reduce((address, { name, append }) => {
-        const value = this.location.address[name];
-        if (!value) return address;
-
-        return `${address} ${value}${append} `;
-      }, '');
-    },
     navigationUrl() {
       return `https://www.google.com/maps/dir/?api=1&destination=${this.address}`;
     },
