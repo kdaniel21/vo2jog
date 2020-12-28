e5bda9d28800d4e8923c1a599a5890b94e9673d9
Daniel Kiss <kissd621@gmail.com>
2020-12-28 09:37:54 +0100

    Encapsulated image avatar

--- components/organizer/edit/information/EditImageUpload.vue
+++ components/organizer/edit/information/EditImageUpload.vue
@@ -2,34 +2,9 @@
   <div id="edit-image-upload">
     <h2 class="title">{{ $t('organizer.info.image') }}</h2>
 
-    <div v-if="!form.image">
-      <b-field class="file">
-        <b-upload v-model="form.image" expanded drag-drop>
-          <section class="section has-text-centered">
-            <p>
-              <b-icon icon="upload" size="is-large" />
-            </p>
-            <p>{{ $t('organizer.info.upload_area') }}</p>
-          </section>
-        </b-upload>
-      </b-field>
-    </div>
-
-    <div v-else>
-      <b-image id="image-view" :src="imagePreview" ratio="4by3" class="mb-3" />
-
-      <b-button
-        v-if="!uploadNew"
-        type="is-primary"
-        icon-left="upload"
-        expanded
-        @click="removeCurrent"
-      >
-        {{ $t('organizer.info.upload_new') }}
-      </b-button>
-
-      <button-right v-else @click="onSave" />
-    </div>
+    <image-upload ref="image-upload" v-model="form.image">
+      <button-right slot="save-button" @click="onSave" />
+    </image-upload>
   </div>
 </template>
 
@@ -40,28 +15,19 @@ export default {
   name: 'EditImageUpload',
   data() {
     return {
-      uploadNew: !this.$store.state.organizer.events.selectedEvent.imageCover,
       form: {
         image: this.$store.state.organizer.events.selectedEvent.imageCover,
       },
     };
   },
-  computed: {
-    imagePreview() {
-      const { image } = this.form;
-      return typeof image === 'object' ? URL.createObjectURL(image) : image;
-    },
-  },
   methods: {
     ...mapActions('organizer/events', ['updateEvent']),
-    removeCurrent() {
-      this.uploadNew = true;
-      this.form.image = null;
-    },
-    onSave() {
+    async onSave() {
       const formData = new FormData();
       formData.append('imageCover', this.form.image, this.form.image.filename);
-      return this.updateEvent(formData);
+      await this.updateEvent(formData);
+
+      this.$refs['image-upload'].resetState();
     },
   },
 };
---
+++ components/organizer/shared/ImageUpload.vue
@@ -0,0 +1,77 @@
+<template>
+  <div id="image-upload">
+    <div v-if="!image">
+      <b-field class="file">
+        <b-upload v-model="image" expanded drag-drop>
+          <section class="section has-text-centered">
+            <p>
+              <b-icon icon="upload" size="is-large" />
+            </p>
+            <p>{{ $t('organizer.info.upload_area') }}</p>
+          </section>
+        </b-upload>
+      </b-field>
+    </div>
+
+    <div v-else>
+      <slot :img="imagePreview">
+        <b-image
+          id="image-view"
+          :src="imagePreview"
+          ratio="4by3"
+          class="mb-3"
+        />
+      </slot>
+
+      <b-button
+        v-if="!uploadNew"
+        type="is-primary"
+        icon-left="upload"
+        expanded
+        @click="removeCurrent"
+      >
+        {{ $t('organizer.info.upload_new') }}
+      </b-button>
+
+      <slot v-else name="save-button" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ImageUpload',
+  // eslint-disable-next-line vue/require-prop-types
+  props: ['value'],
+  data() {
+    return {
+      uploadNew: !this.value,
+    };
+  },
+  computed: {
+    image: {
+      get() {
+        return this.value;
+      },
+      set(val) {
+        this.$emit('input', val);
+      },
+    },
+    imagePreview() {
+      const { image } = this;
+      return typeof image === 'object' ? URL.createObjectURL(image) : image;
+    },
+  },
+  methods: {
+    removeCurrent() {
+      this.uploadNew = true;
+      this.image = null;
+    },
+    resetState() {
+      this.uploadNew = false;
+    },
+  },
+};
+</script>
+
+<style></style>
