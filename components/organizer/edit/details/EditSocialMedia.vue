3a9aeddeb02713ca24ab9bfb6f15820e09e05dc6
Daniel Kiss <kissd621@gmail.com>
2020-12-28 10:49:51 +0100

    Refactored code to use shared component

--- components/organizer/edit/details/EditSocialMedia.vue
+++ components/organizer/edit/details/EditSocialMedia.vue
@@ -2,22 +2,34 @@
   <div id="edit-social-media">
     <h2 class="title">{{ $t('organizer.details.social_media') }}</h2>
 
-    <social-media-items @edit="item => (editedItem = item)" />
-
-    <new-social-media-item
-      :edited-item="editedItem"
-      @cancel="editedItem = null"
+    <social-media-list
+      :items="selectedEvent.socialMedia"
+      :create-function="addItem"
+      :update-function="updateItem"
+      :delete-function="showDeleteConfirm"
     />
   </div>
 </template>
 
 <script>
+import { mapState, mapActions } from 'vuex';
+
 export default {
   name: 'EditSocialMedia',
   data() {
     return {
       editedItem: null,
     };
+  },
+  computed: {
+    ...mapState('organizer/events', ['selectedEvent']),
+  },
+  methods: {
+    ...mapActions('organizer/events', [
+      'addItem',
+      'updateItem',
+      'showDeleteConfirm',
+    ]),
   },
 };
 </script>
