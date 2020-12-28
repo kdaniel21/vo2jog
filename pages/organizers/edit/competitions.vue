eeeef78b053406cd9e24fa65309749ca7accb3b1
Daniel Kiss <kissd621@gmail.com>
2020-12-28 06:36:38 +0100

    Added new competitions edit page and removed old

---
+++ pages/organizers/edit/competitions.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="container mb-6">
+    <h1 class="title mb-2">{{ $t('organizer.competitions.title') }}</h1>
+    <p>{{ $t('organizer.competitions.description') }}</p>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
